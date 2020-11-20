4e0669a9c4d1fb08b9022d21b2e324833ed3b8db
bdavis160 <60025535+bdavis160@users.noreply.github.com>
2020-11-20 15:15:03 -0800

    basic google maps added gmaps.pug is a maps test

--- scripts/api.js
+++ scripts/api.js
@@ -5,18 +5,20 @@ var request = require('request-promise');
 var base = 'https://api.openchargemap.io/v3/?'; //'https://api.openchargemap.io/v3/';
 var key = '8bc4f8db-272f-458b-82f7-c052a5c53c9a';
 var url = base + 'key=' + key;
-var temp = "https://api.openchargemap.io/v3/poi/?key=8bc4f8db-272f-458b-82f7-c052a5c53c9a&output=json&countrycode=US&maxresults=10";
+var temp = "https://api.openchargemap.io/v3/poi/?key=8bc4f8db-272f-458b-82f7-c052a5c53c9a&output=json&countrycode=US&maxresults=20";
+var gurl = "https://maps.googleapis.com/maps/api/js?key=AIzaSyC6_0ao-hnjTGFAdtsfRhIKhm_BK2zhKMg&libraries=places&callback=initMap";
 app.set('views', __dirname + '/../views');
 app.set('view engine', 'pug');
 app.use(express.json());
+app.use(express.urlencoded());
 function options(options) {
     var output = "";
     var o = "";
     for (o in options)
         if (output == "")
-            output = o;
+            output = options[o];
         else
-            output += o;
+            output += "&" + options[o];
     return output;
 }
 var results = [];
@@ -24,16 +26,32 @@ var results = [];
 request(temp, function (error, response, body) {
     if (!error && response.statusCode == 200) {
         var json_body = JSON.parse(body);
-        console.log(json_body);
-        /*let row : string = ''
-        for(row in json_body)
-            {
-                results.push({'name' : json_body[row].name, 'population' : json_body[row].population});//TODO Style correctly
-            }*/
+        //console.log(json_body)
+        return json_body;
     }
 });
+var gmaps = request(gurl, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+        return body;
+    }
+});
+var pos;
+var map;
+function initMap() {
+    // Set the default location and initialize all variables
+    pos = { lat: -33.857, lng: 151.213 };
+    map = new gmaps.google.maps.Map(document.getElementById('map'), {
+        center: pos,
+        zoom: 15
+    });
+}
 app.get('/', function (req, res) {
     res.render('index', {
+        Title: 'Home'
+    });
+});
+app.get('/gmaps', function (req, res) {
+    res.render('gmaps', {
         Title: 'Home'
     });
 });
@@ -58,11 +76,14 @@ app.get('/search', function (req, res) {
     });
 });
 app.post('/search_results', function (req, res) {
-    res.render('search_results', {
-        Title: 'Results'
-    });
-});
-app.post('/directions_results', function (req, res) {
+    console.log(req.body);
+    //todo: search api for location and charger
+    var distance = 'distance=' + req.body.radius;
+    var country = 'countrycode=US';
+    var distance_u = 'distanceunit=Miles';
+    //TODO IMPORT GOOGLE MAPS LAT/LONG
+    var ret = options([url, distance, country, distance_u]);
+    console.log(ret);
     res.render('directions_results', {
         Title: 'Results'
     });
@@ -70,37 +91,3 @@ app.post('/directions_results', function (req, res) {
 app.listen(port, function () {
     console.log("Server running at http://localhost:" + port);
 });
-/*const express : any = require('express');
-const app : any = express();
-const port : number  = parseInt(process.env.PORT) || 5000;
-const request : any = require('request');
-const url : string = 'https://restcountries.eu/rest/v2/all';
-
-app.use(express.json());
-
-app.get('/main', (req : any, res : any) => {
-    let results : object [] = [];
-    let row : string = "";
-    
-    request(url, function (error : number, response : any, body : string) {
-    if (!error && response.statusCode == 200) {
-        let json_body : any = JSON.parse(body);
-        
-        for(row in json_body)
-            {
-                results.push({'name' : json_body[row].name, 'population' : json_body[row].population});//TODO Style correctly
-            }
-
-    }
-    res.render('index', {
-    title: 'Main',
-    heading: 'Welcome to the Main Page',
-    data: results
-    });
-    });
-});
-
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
-});
-*/ 
--- scripts/api.ts
+++ scripts/api.ts
@@ -5,11 +5,14 @@ const request : any = require('request-promise');
 const base : string = 'https://api.openchargemap.io/v3/?'; //'https://api.openchargemap.io/v3/';
 const key : string = '8bc4f8db-272f-458b-82f7-c052a5c53c9a'
 const url : string = base + 'key=' + key;
-const temp : string = "https://api.openchargemap.io/v3/poi/?key=8bc4f8db-272f-458b-82f7-c052a5c53c9a&output=json&countrycode=US&maxresults=10";
+const temp : string = "https://api.openchargemap.io/v3/poi/?key=8bc4f8db-272f-458b-82f7-c052a5c53c9a&output=json&countrycode=US&maxresults=20";
+const gurl : string = "https://maps.googleapis.com/maps/api/js?key=AIzaSyC6_0ao-hnjTGFAdtsfRhIKhm_BK2zhKMg&libraries=places&callback=initMap"
+
 app.set('views', __dirname + '/../views');
 app.set('view engine', 'pug');
 
 app.use(express.json());
+app.use(express.urlencoded());
 
 function options(options : string []) : string {
 
@@ -17,9 +20,9 @@ function options(options : string []) : string {
     let o : string = ""
     for(o in options)
         if (output == "")
-            output = o;
+            output = options[o];
         else
-            output += o;
+            output += "&" + options[o];
 
     return output;
 
@@ -28,21 +31,42 @@ function options(options : string []) : string {
 let results : Object[] = [];
 
 //Figure out charger info/route info
-request(temp, function (error : number, response : any, body : any) : void{
+request(temp, function (error : number, response : any, body : any) : Object{
     if (!error && response.statusCode == 200) {
         let json_body : Object = JSON.parse(body);
-        console.log(json_body)
-        /*let row : string = ''
-        for(row in json_body)
-            {
-                results.push({'name' : json_body[row].name, 'population' : json_body[row].population});//TODO Style correctly               
-            }*/
+        //console.log(json_body)
 
+        return json_body;
     }
 });
 
+let gmaps = request(gurl, function (error : number, response : any, body : any) : void{
+    if (!error && response.statusCode == 200) {
+        return body;
+    }
+});
+
+let pos : object;
+let map : any;
+function initMap() {
+      // Set the default location and initialize all variables
+      pos = { lat: -33.857, lng: 151.213 };
+      map = new gmaps.google.maps.Map(document.getElementById('map'), {
+        center: pos,
+        zoom: 15
+    });
+}
+
 app.get('/', (req, res) => {
+
     res.render('index', {
+        Title : 'Home'
+    });
+});
+
+app.get('/gmaps', (req, res) => {
+
+    res.render('gmaps', {
         Title : 'Home'
     });
 });
@@ -66,18 +90,23 @@ app.get('/about', (req, res) => {
 });
 
 app.get('/search', (req, res) => {
+
     res.render('search', {
         Title : 'Search'
     });
 });
 
 app.post('/search_results', (req, res) => {
-    res.render('search_results', {
-        Title : 'Results'
-    });
-});
 
-app.post('/directions_results', (req, res) => {
+    console.log(req.body);
+    //todo: search api for location and charger
+    let distance :string = 'distance=' + req.body.radius;
+    let country : string = 'countrycode=US';
+    let distance_u : string = 'distanceunit=Miles';
+    //TODO IMPORT GOOGLE MAPS LAT/LONG
+    let ret = options([url,distance, country, distance_u]);
+    console.log(ret);
+
     res.render('directions_results', {
         Title : 'Results'
     });
@@ -89,37 +118,3 @@ app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
     
-/*const express : any = require('express');
-const app : any = express();
-const port : number  = parseInt(process.env.PORT) || 5000;
-const request : any = require('request');
-const url : string = 'https://restcountries.eu/rest/v2/all';
-
-app.use(express.json());
-
-app.get('/main', (req : any, res : any) => {
-    let results : object [] = [];
-    let row : string = "";
-    
-    request(url, function (error : number, response : any, body : string) {
-    if (!error && response.statusCode == 200) {
-        let json_body : any = JSON.parse(body);
-        
-        for(row in json_body)
-            {
-                results.push({'name' : json_body[row].name, 'population' : json_body[row].population});//TODO Style correctly
-            }
-
-    }
-    res.render('index', {
-    title: 'Main',
-    heading: 'Welcome to the Main Page',
-    data: results
-    });
-    });
-});
-
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
-});
-*/
