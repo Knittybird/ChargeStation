0b16854f63d763715fb113909f1164eecd4459a1
bdavis160 <60025535+bdavis160@users.noreply.github.com>
2020-11-25 10:27:23 -0800

    prebuild script updated

--- public/api_link.js
+++ public/api_link.js
@@ -1,47 +1,38 @@
-const mapKey  = process.env.MAP_KEY
-const request = require('request-promise');
-const googleMapsClient = require('@google/maps').createClient({
-    key:mapKey
+var mapKey = process.env.MAP_KEY;
+var request = require('request-promise');
+var googleMapsClient = require('@google/maps').createClient({
+    key: mapKey
 });
-
 //console.log(process.env.OPENCHARGE_KEY);
-    
-
-    function options(options) {
-
-        let output = "";
-        let o  = ""
-        for(o in options)
-            if (output == "")
-                output = options[o];
-            else
-                output += "&" + options[o];
-    
-        return output;
-    
-    }
-    module.exports = {
-    addr : function addr_to_latlng(addr, url, verbose, output, incl_comm, max_results, compact, distance, distance_u) {
-    //let out_url : string = url + output;
-    googleMapsClient.geocode({
-    address: addr
-    }, function(err, response ) {
-    if (!err) {
-        let lat = "latitude="+response.json.results[0].geometry.location['lat'];
-        let lng = "longitude="+response.json.results[0].geometry.location['lng'];
-        let ret = options([url,verbose, output, incl_comm, max_results, compact, lat, lng,distance, distance_u]);
-
-        console.log(ret);
-
-        request(ret, function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-                let json_body = JSON.parse(body);
-                console.log(json_body)
-        
-                //return json_body;
+function options(options) {
+    var output = "";
+    var o = "";
+    for (o in options)
+        if (output == "")
+            output = options[o];
+        else
+            output += "&" + options[o];
+    return output;
+}
+module.exports = {
+    addr: function addr_to_latlng(addr, url, verbose, output, incl_comm, max_results, compact, distance, distance_u) {
+        googleMapsClient.geocode({
+            address: addr
+        }, function (err, response) {
+            if (!err) {
+                var lat = "latitude=" + response.json.results[0].geometry.location['lat'];
+                var lng = "longitude=" + response.json.results[0].geometry.location['lng'];
+                var ret = options([url, verbose, output, incl_comm, max_results, compact, lat, lng, distance, distance_u]);
+                console.log(ret);
+                request(ret, function (error, response, body) {
+                    if (!error && response.statusCode == 200) {
+                        var json_body = JSON.parse(body);
+                        console.log(json_body);
+                        //we'll need to return an object with the relevant data
+                        //return json_body;
+                    }
+                });
             }
         });
     }
-    });
-    }
-    };
+};
