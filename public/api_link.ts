0b9753bf72d9939c773063284398149a2d9d9d15
bdavis160 <60025535+bdavis160@users.noreply.github.com>
2020-11-26 10:56:19 -0800

    Returns still don't work

--- public/api_link.ts
+++ public/api_link.ts
@@ -22,9 +22,10 @@ const googleMapsClient = require('@google/maps').createClient({
     
     }
     module.exports = {
-    addr : function addr_to_latlng(addr : any, url : string, verbose : string, output : string, incl_comm : string, max_results : string, compact : string, distance : string, distance_u : string) {
-    let results : object[] = [];
-    googleMapsClient.geocode({
+    addr : function addr_to_latlng(addr : any, url : string, verbose : string, output : string, incl_comm : string, max_results : string, compact : string, distance : string, distance_u : string, res : any, req : any) {
+    let t : object[] = [];
+
+    let gcode : any = googleMapsClient.geocode({
     address: addr
     }, function(err : any, response : any ) {
     if (!err) {
@@ -32,22 +33,28 @@ const googleMapsClient = require('@google/maps').createClient({
         let lng : string = "longitude="+response.json.results[0].geometry.location['lng'];
         let ret : string = options([url,verbose, output, incl_comm, max_results, compact, lat, lng,distance, distance_u]);
 
-        console.log(ret);
+       // console.log(ret);
 
         request(ret, function (error : any, response : any, body : any) : any{
             if (!error && response.statusCode == 200) {
                 let json_body = JSON.parse(body);
-                let jb :any = "";
-
+                let jb : any = 0;
+                t = [{"addr" : json_body[jb].AddressInfo['AddressLine1']}];
+                //console.log(json_body[0].AddressInfo['AddressLine1'])
                 for(jb in json_body)
                 {
-                    results.push({"addr" : json_body[jb].AddressInfo['AddressLine1']})
+                    if(jb!==0) {
+                        t.push({"addr" : json_body[jb].AddressInfo['AddressLine1']})
+                        //console.log(results);
+                    }
+                    
                 }
-                console.log(json_body)
-                
-                //we'll need to return an object with the relevant data
-                //return json_body;
+            
             }
+
+            /*res.render('search_results', {
+                Title : 'Results'
+            });*/
         });
     }
     });
