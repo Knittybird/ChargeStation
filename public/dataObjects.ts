d863113c9f84725cdede54b45c27ce207a53e392
Robin <rgearn@pdx.edu>
2020-11-27 20:53:06 -0800

    merge changes with main

--- public/dataObjects.ts
+++ public/dataObjects.ts
@@ -1,12 +1,7 @@
 /* defines objects to pass data from backend to frontend  */
 
-<<<<<<< HEAD
-//module.exports = {
-  export interface Charger {
-=======
 export interface Charger{
   id: string;
->>>>>>> 6543677588112b8a85358fd6433e87dabb362965
   address: {
     addressLine: string;
     title: string;
@@ -18,10 +13,4 @@ export interface Charger{
     lat?: string;
     lng?: string;
   }
-<<<<<<< HEAD
 }
-//}
-=======
-  
-}
->>>>>>> 6543677588112b8a85358fd6433e87dabb362965
