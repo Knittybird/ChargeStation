1be540ee0ce048d51dfb92d128ee0d73858333ac
Robin <rgearn@pdx.edu>
2020-11-29 09:07:56 -0800

    added test

--- __tests__/details.test.ts
+++ __tests__/details.test.ts
@@ -9,20 +9,76 @@ function sum (a,b) {return a + b};
 
 
 
-test('checks that the output matches', () => {
-  const input: any = {
-      "DataProvider": {
-        "WebsiteURL": "http://www.carstations.com",
-        "DataProviderStatusType": {
-          "IsProviderEnabled": true,
-          "ID": 20,
-          "Title": "Automated Import"
-        },
-        "IsRestrictedEdit": false,
-        "DateLastImported": "2017-11-04T14:07:02.687Z",
-        "ID": 15,
-        "Title": "CarStations.com"
+  const input1: any = {
+      "UsageType": {
+        "ID": 1,
+        "Title": "Public"
       },
+      "StatusType": {
+        "IsUserSelectable": true,
+        "ID": 0,
+        "Title": "Unknown"
+      },
+      "SubmissionStatus": {
+        "IsLive": true,
+        "ID": 100,
+        "Title": "Imported and Published"
+      },
+      "IsRecentlyVerified": false,
+      "ID": 9215,
+      "UUID": "EFB22A04-3CCF-4F66-B146-AA54F495E440",
+      "DataProviderID": 15,
+      "DataProvidersReference": "10397",
+      "OperatorID": 14,
+      "UsageTypeID": 1,
+      "AddressInfo": {
+        "ID": 9112,
+        "Title": "150 S Beavercreek Rd",
+        "AddressLine1": "150 S Beavercreek Rd",
+        "Town": "Oregon City",
+        "StateOrProvince": "OR",
+        "Postcode": "97045",
+        "CountryID": 2,
+        "Country": {
+          "ISOCode": "US",
+          "ContinentCode": "NA",
+          "ID": 2,
+          "Title": "United States"
+        },
+        "Latitude": 45.334205,
+        "Longitude": -122.59962,
+        "ContactTelephone1": "503-892-7345 ",
+        "RelatedURL": "http://carstations.com/10397",
+        "DistanceUnit": 0
+      },
+      "Connections": [
+        {
+          "ID": 6896,
+          "ConnectionTypeID": 9,
+          "ConnectionType": {
+            "IsDiscontinued": false,
+            "IsObsolete": false,
+            "ID": 9,
+            "Title": "NEMA 5-20R"
+          },
+          "Reference": "NEMA5",
+          "LevelID": 1,
+          "Level": {
+            "Comments": "Under 2 kW, usually domestic socket types",
+            "IsFastChargeCapable": false,
+            "ID": 1,
+            "Title": "Level 1 : Low (Under 2kW)"
+          }
+        }
+      ],
+      "NumberOfPoints": 1,
+      "StatusTypeID": 0,
+      "DataQualityLevel": 2,
+      "DateCreated": "2012-01-23T15:12:00Z",
+      "SubmissionStatusTypeID": 100
+    };
+    
+    const input2: any = {
       "OperatorInfo": {
         "WebsiteURL": "http://www.shorepower.com/",
         "Comments": "Imported by ADFC import",
@@ -93,12 +149,12 @@ test('checks that the output matches', () => {
       ],
       "NumberOfPoints": 1,
       "StatusTypeID": 0,
-      "DateLastStatusUpdate": "2017-11-04T14:05:00Z",
       "DataQualityLevel": 2,
       "DateCreated": "2012-01-23T15:12:00Z",
       "SubmissionStatusTypeID": 100
     };
 
+test('checks that the output matches all fields', () => {
   const output: Charger = {
     "id": "9215",
     "address": {
@@ -120,7 +176,34 @@ test('checks that the output matches', () => {
   };
 
   // expect(sum(3, 4)).toEqual(7);
-  expect(parseDetailData(input)).toEqual(output);
+  expect(parseDetailData(input2)).toEqual(output);
+ });
+
+
+test('checks that operator fields blank other fields filled', () => {
+  const output: Charger = {
+    "id": "9215",
+    "address": {
+      "addressLine": "150 S Beavercreek Rd",
+      "title": "150 S Beavercreek Rd",
+      "town": "Oregon City",
+      "state": "OR",
+      "postalCode": "97045"
+    },
+    "connectionType": [
+      {
+        "typeId": 9,
+        "levelId": 1
+      }
+    ],
+    "usage": "1",
+  };
+
+  // expect(sum(3, 4)).toEqual(7);
+  const charger: Charger = parseDetailData(input1);
+  expect(charger).toEqual(output);
+  expect(charger.operatorTitle).toBeUndefined;
+  expect(charger.website).toBeUndefined;
  });
 
 
