8ca9fc1eae1c28dee14e7949a31ae1d3492a139b
Robin <rgearn@pdx.edu>
2020-11-29 04:24:39 -0800

    removed .js files

--- __tests__/details.test.js
+++
@@ -1,8 +0,0 @@
-const { defaultMaxListeners } = require("node-fetch/node_modules/form-data")
-
-const fetchDetails = require('../dist/public/defaultMaxListeners.js);
-const fetch = require('node-fetch');
-
-jest.mock('fetch');
-
-it('renders')
