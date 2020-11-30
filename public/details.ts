d27c7feb347a3c38f318842c733540e3255699d5
Robin <rgearn@pdx.edu>
2020-11-29 17:55:07 -0800

    catch empty detail results

--- public/details.ts
+++ public/details.ts
@@ -15,7 +15,15 @@ export async function fetchCharge (res, url: string, id: string): Promise<any> {
 
     const charger: Charger = parseDetailData(data[0]);
     // console.log(JSON.stringify(charger, null, 2));
-    res.render('details', {charger: charger})
+    if (charger) {
+      res.render('details', {charger: charger})
+    } else {
+      res.render("errors/no_results", {
+        title : '0 Results',
+        desc : 'Oops! Looks like we could not find what you were looking for...',
+        search : "Search Again?"
+      });
+    }
 
   } catch (error) {
     // TODO: output error page
@@ -28,37 +36,40 @@ export function parseDetailData (chargeData: any): Charger  {
 
     // copy conections into array
     let connections: Connection[] = [];
+    if (chargeData) {
+      if (chargeData.Connections) {
+        chargeData.Connections.forEach (connect => {
+          let connector: Connection = {typeId: 0, levelId: 0};
+          connector.typeId = connect.ConnectionTypeID;
+          connector.levelId = connect.LevelID;
+          connections.push(connector);
+        });
+      }
 
-    if (chargeData.Connections) {
-      chargeData.Connections.forEach (connect => {
-        let connector: Connection = {typeId: 0, levelId: 0};
-        connector.typeId = connect.ConnectionTypeID;
-        connector.levelId = connect.LevelID;
-        connections.push(connector);
-      });
-    }
+      //copy data into charger record
+      let charger : Charger = {
+        id: chargeData.ID.toString(),
+        address: {
+          addressLine: chargeData.AddressInfo.AddressLine1,
+          title: chargeData.AddressInfo.Title,
+          town: chargeData.AddressInfo.Town,
+          state: chargeData.AddressInfo.StateOrProvince,
+          postalCode: chargeData.AddressInfo.Postcode,
+        },
+        connectionType: connections,
+        usage: "",
+      }
 
-    //copy data into charger record
-    let charger : Charger = {
-      id: chargeData.ID.toString(),
-      address: {
-        addressLine: chargeData.AddressInfo.AddressLine1,
-        title: chargeData.AddressInfo.Title,
-        town: chargeData.AddressInfo.Town,
-        state: chargeData.AddressInfo.StateOrProvince,
-        postalCode: chargeData.AddressInfo.Postcode,
-      },
-      connectionType: connections,
-      usage: "",
+      if (chargeData.OperatorInfo){
+        charger.operatorTitle = chargeData.OperatorInfo.Title,
+        charger.website = chargeData.OperatorInfo.WebsiteURL
+      }
+      if (chargeData.UsageTypeID){
+        charger.usage = chargeData.UsageTypeID.toString();
+      }
+      return charger;
+    } else {
+      return undefined;
     }
-
-    if (chargeData.OperatorInfo){
-      charger.operatorTitle = chargeData.OperatorInfo.Title,
-      charger.website = chargeData.OperatorInfo.WebsiteURL
-    }
-    if (chargeData.UsageTypeID){
-      charger.usage = chargeData.UsageTypeID.toString();
-    }
-    return charger;
 }
 
